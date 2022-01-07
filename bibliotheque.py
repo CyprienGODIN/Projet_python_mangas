ed457a7511aab3cef22100381385f5dfd1e801ee
Théo Dalban <theo.dalban@gmail.com>
2022-01-07 18:30:15 +0100

    mise à jour bibliotheque

--- bibliotheque.py
+++ bibliotheque.py
@@ -1,8 +1,13 @@
+import itertools
+
 class Bibliotheque:
-    
+    # Iteration de l'id des bibliotheques
+    newid = itertools.count().next
+
     # Constructeur
-    def __init__(self,type,nombre,contenu):
+    def __init__(self,id=int,type="",nombre=int,contenu=dict()):
         # definition des attributs
+        self.id = Bibliotheque.newid()
         self.type = type
         self.nombre = nombre
         self.contenu = contenu
