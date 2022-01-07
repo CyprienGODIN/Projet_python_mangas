f70753f91ac9fd56b5c061963fde98e5883ffaac
Théo Dalban <theo.dalban@gmail.com>
2022-01-07 18:20:59 +0100

    Essai de mettre dans dict

---
+++ main.py
@@ -0,0 +1,11 @@
+# Importation
+import csv
+import pprint
+
+# CSV -> dictionnaire    
+# fieldnames = ['mal_id', 'rank', 'title', 'url', 'image_url', 'type', 'episodes', 'start_date', 'end_date', 'members', 'score']
+reader = csv.DictReader(open('anime.csv', 'r'))
+dict_list = []
+for line in reader:
+    dict_list.append(line)
+pprint.pprint(dict_list)
